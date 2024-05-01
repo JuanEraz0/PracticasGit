bbab43149dc6475e16ca6192ceeb4da0126dee4d
JuanErazo <juanerazo.fi@gmail.com>
2024-04-30 19:46:49 -0600

    SE modifica localmente y se manda a repo

--- index.cpp
+++ index.cpp
@@ -1,1 +1,14 @@
 /* COMENTARIO PRUEBA, MODIFICANDO ARCHIVO DESDE GITHUB PARA probar comando stash*/
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hola Mundo"<<endl;
+
+    return 0;
+
+
+
+
+}
